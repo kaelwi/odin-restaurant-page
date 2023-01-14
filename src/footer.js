b43fda3fceba392958e0c2854b2428216796272f
Karoline Elisabeth Wild <karoline.elisabeth.wild@seznam.cz>
2023-01-14 13:55:36 +0100

    Add container for main content

--- src/footer.js
+++ src/footer.js
@@ -5,6 +5,10 @@ function footer() {
   const footer = document.createElement('footer');
   const p = document.createElement('p');
   p.textContent = "Created by kaelwi 01/2023";
+
+  const hr = document.createElement('hr');
+  
+  footer.appendChild(hr);
   footer.appendChild(p);
   parent.appendChild(footer);
 }
--- src/home.js
+++ src/home.js
@@ -1,8 +1,14 @@
 function home() {
   const content = document.getElementById('content');
-  const p = document.createElement('p');
-  p.innerText = 'Test';
-  content.appendChild(p);
+
+  const container = document.createElement('div');
+  container.id = 'container';
+
+  const intro = document.createElement('p');
+  intro.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc dui, cursus id enim viverra, ornare feugiat mauris. Nulla a laoreet tortor. Nunc a rutrum massa. Maecenas ut vulputate urna. Nulla in odio et odio semper pellentesque. Pellentesque habitant morbi tristique senectur et netus et malesuada fames ac turpis egestas.';
+  
+  container.appendChild(intro);
+  content.appendChild(container);
 }
 
 export default home;
